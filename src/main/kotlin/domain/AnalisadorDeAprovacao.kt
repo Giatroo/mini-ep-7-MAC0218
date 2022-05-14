1f0eb50b66a7382af1f834b5fc33d8f10dbea390
Lucas Paiolla <lucaspaiolla@gmail.com>
2022-05-14 11:01:01 -0300

    final commit
    as the previous commit indicated, this commit implements the code as it
    should be. The previous commit was already passing all the tests, but
    this is the intended code.

--- src/main/kotlin/domain/AnalisadorDeAprovacao.kt
+++ src/main/kotlin/domain/AnalisadorDeAprovacao.kt
@@ -1,9 +1,10 @@
 package domain
 
 import domain.criterios.CriterioDeAprovacao
+import domain.criterios.Basico
 
 class AnalisadorDeAprovacao {
-    private var criterio : CriterioDeAprovacao? = null
+    private var criterio : CriterioDeAprovacao = Basico()
 
     fun defineCriterio(criterio: CriterioDeAprovacao) {
         this.criterio = criterio
@@ -12,8 +13,8 @@ class AnalisadorDeAprovacao {
     fun fechaBoletim(boletim: Boletim) : BoletimFechado {
         val mediaEPs = boletim.mediaEPs
         val mediaMiniEPs = boletim.mediaMiniEPs
-        val mediaFinal = 7.0
-        val foiAprovado = true
+        val mediaFinal = this.criterio.mediaFinal(boletim)
+        val foiAprovado = this.criterio.estaAprovado(boletim)
 
 
         return BoletimFechado(mediaEPs=mediaEPs, mediaMiniEPs=mediaMiniEPs, mediaFinal, foiAprovado)
